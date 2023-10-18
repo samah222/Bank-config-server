9d8cb546a2b2445e9b5ba7d79e0d5f5494ab05fd
Samah Mahdi <samahmahdi22@gmail.com>
2023-10-18 15:17:18 +0400

    Config server for Bank microservices

---
+++ src/test/java/com/samahtech/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samahtech.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
